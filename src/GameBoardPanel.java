d5e84a523076bfed328d5a4675730a0b028c2795
maulinanl <61910881+maulinanl@users.noreply.github.com>
2023-12-14 23:46:37 +0700

    13

--- src/GameBoardPanel.java
+++ src/GameBoardPanel.java
@@ -1,3 +1,13 @@
+/**
+ * ES234317-Algorithm and Data Structures
+ * Semester Ganjil, 2023/2024
+ * Group Capstone Project
+ * Group #9
+ * 1 - 5026221131 - Maulina Nur Laila
+ * 2 - 5026221172 - Arya Putra Tsabitah Firjatulloh
+ * 3 - 5026221179 - Kadek Mawar Kumala Dewi
+ */
+
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
