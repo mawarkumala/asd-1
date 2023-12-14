6b93778bfea2e0461fd101b64e7857299b5839d4
maulinanl <61910881+maulinanl@users.noreply.github.com>
2023-12-14 22:57:40 +0700

    11

--- src/CellStatus.java
+++ src/CellStatus.java
@@ -8,15 +8,9 @@
  * 3 - 5026221179 - Kadek Mawar Kumala Dewi
  */
 
-/**
- * An enumeration of constants to represent the status
- * of each cell.
- */
 public enum CellStatus {
-    GIVEN,         // clue, no need to guess
-    TO_GUESS,      // need to guess - not attempted yet
-    CORRECT_GUESS, // need to guess - correct guess
-    WRONG_GUESS    // need to guess - wrong guess
-    // The puzzle is solved if none of the cells have
-    //  status of TO_GUESS or WRONG_GUESS
+    GIVEN,        
+    TO_GUESS,    
+    CORRECT_GUESS, 
+    WRONG_GUESS  
 }
