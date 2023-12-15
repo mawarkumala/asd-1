be211b6f9235631e85d7767b56f96cdd6d333988
aryaptf <148344372+aryaptf@users.noreply.github.com>
2023-12-15 08:17:48 +0700

    Update SudokuMain.java

--- src/SudokuMain.java
+++ src/SudokuMain.java
@@ -11,10 +11,14 @@
 import javax.swing.*;
 import java.awt.*;
 
+//Mendeklarasikan kelas SudokuMain yang merupakan turunan dari kelas JFrame, yang digunakan untuk membuat frame GUI.
 public class SudokuMain extends JFrame {
     private static final long serialVersionUID = 1L;  // to prevent serial warning
+    // untuk gambar boardnya
     GameBoardPanel board = new GameBoardPanel();
+    //untuk membuat button new game
     JButton btnNewGame = new JButton("New Game");
+    //timer
     JLabel timerLabel = new JLabel("Timer: 0 seconds");
     private Timer timer;
     private int seconds;
@@ -24,32 +28,40 @@ public class SudokuMain extends JFrame {
         JOptionPane.showMessageDialog(null, "Welcome! click OK to start game! Sudoko");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         Container cp = getContentPane();
+        //inisialisasi layout frame
         cp.setLayout(new BorderLayout());
+        //Menambahkan panel permainan dan panel tombol ke frame dengan menggunakan layout BorderLayout.
         cp.add(board, BorderLayout.CENTER);
-        
+
+        // Add buttons and timer label to the right to re-start the game via board.newGame() and restart
         JPanel buttonPanel = new JPanel();
         buttonPanel.add(btnNewGame);
         buttonPanel.add(timerLabel);
         cp.add(buttonPanel, BorderLayout.EAST);
-        
+
+        //Menambahkan action listener untuk tombol "New Game" yang akan memanggil metode startNewGame().
         btnNewGame.addActionListener(e -> startNewGame());
-        
+
+        // Initialize the game board and timer
         initializeTimer();
         startTimer();
         board.newGame();
 
-        pack();   
+        pack();   // Mempaketkan komponen-komponen UI untuk menyesuaikan ukuran frame.
+        //Mengatur posisi frame, judul, dan membuat frame terlihat.
         setLocationRelativeTo(null);
         setTitle("Sudoku");
         setVisible(true);
         setResizable(false);
     }
 
+    // Method to start a new game
     private void startNewGame() {
         restartTimer();
         board.newGame();
     }
 
+    // Method to initialize the timer
     private void initializeTimer() {
         seconds = 0;
         timer = new Timer(1000, e -> {
@@ -58,10 +70,12 @@ public class SudokuMain extends JFrame {
         });
     }
 
+    // Method to start the timer
     private void startTimer() {
         timer.start();
     }
 
+    // Method to restart the timer
     private void restartTimer() {
         timer.stop();
         seconds = 0;
@@ -69,11 +83,14 @@ public class SudokuMain extends JFrame {
         timer.start();
     }
 
+    // Method to update the timer label
     private void updateTimerLabel() {
         timerLabel.setText("Timer: " + seconds + " seconds");
     }
 
+    /** The entry main() entry method */
     public static void main(String[] args) {
+        // Run GUI codes in the Event-Dispatching thread for thread safety
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
                 new SudokuMain(); 
