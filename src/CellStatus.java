06283cb53163b7d7aaac96fdf75b0a3a5c578067
aryaptf <148344372+aryaptf@users.noreply.github.com>
2023-12-15 08:02:02 +0700

    Update CellStatus.java

--- src/CellStatus.java
+++ src/CellStatus.java
@@ -8,9 +8,15 @@
  * 3 - 5026221179 - Kadek Mawar Kumala Dewi
  */
 
+/**
+ * An enumeration of constants to represent the status
+ * of each cell.
+ */
 public enum CellStatus {
-    GIVEN,        
-    TO_GUESS,    
-    CORRECT_GUESS, 
-    WRONG_GUESS  
+    GIVEN,            // clue, no need to guess
+    TO_GUESS,         // need to guess - not attempted yet
+    CORRECT_GUESS,    // need to guess - correct guess
+    WRONG_GUESS       // need to guess - wrong guess
+    // The puzzle is solved if none of the cells have
+    //  status of TO_GUESS or WRONG_GUESS
 }
